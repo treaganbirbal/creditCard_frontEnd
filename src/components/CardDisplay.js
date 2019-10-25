6a82e5c78daf331feb9e762e64d9e7b8da233352
Treagan Birbal <treaganbirbal@pursuit.org>
2019-10-25 17:55:25 -0400

    pass props to card display

--- src/components/CardDisplay.js
+++ src/components/CardDisplay.js
@@ -4,8 +4,15 @@ import React from 'react';
 const CardDisplay = (props) => {
     return (
         <div className='card-display'>
-            <img className='logo' src="" alt=""/>
+         <div className='card'>
             <span className='card-name'>{props.cardInfo.firstName}</span>
+            <span className='card-name'>{props.cardInfo.lastName}</span>
+          <div className='date'>
+            <span>{props.cardInfo.expMonth}</span>
+            <span>{props.cardInfo.expDay}</span>
+            <span>{props.cardInfo.expYear}</span>
+          </div>
+         </div>
         </div>
     )
 }
